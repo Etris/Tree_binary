78054dbc23e82b156aa4da63f4d4581b18aa9d65
Remi <remigiuszgebka@gmail.com>
2017-03-20 13:55:20 +0100

    bst & avl class headers

--- Tree/avlNode.h
+++ Tree/avlNode.h
@@ -1,8 +1,28 @@
 #pragma once
+struct aNode {
+	aNode *prev, *left, *right;
+	int key, bf;
+};
 class avlNode
 {
+private:
+	aNode * root;
+	int count;
+	int height;
+	aNode * rotateLeft(aNode *);
+	aNode * rotateRight(aNode *);
+	aNode * rotateLR(aNode *);
+	aNode * rotateRL(aNode *);
 public:
+	//constr & deconst
 	avlNode();
 	~avlNode();
+	//settery & gettery
+	int getCount();
+	int getHeight();
+	void setCount(int);
+	void setHeight(int);
+	//class methods
+	
 };
 
--- Tree/binaryTNode.h
+++ Tree/binaryTNode.h
@@ -1,8 +1,38 @@
 #pragma once
+struct Node {
+	Node * prev, *left, *right;
+	int key;
+};
 class binaryTNode
 {
+private:
+	Node * root;
+	int count;
+	int height;
 public:
+	//constructor & dec
 	binaryTNode();
 	~binaryTNode();
+	//settery & gettery
+	int getCount();
+	int getHeight();
+	void setCount(int);
+	void setHeight(int);
+	//class methods
+	bool insertNode(Node *);
+	Node * search(int);
+	int maxKey(Node *);
+	int minKey(Node *);
+	Node * maxNode(Node *);
+	Node * minNode(Node *);
+	Node * prev(Node *);
+	Node * succ(Node *);
+	Node * remove(Node*);
+	void preOrder(Node*);
+	void inOrder(Node*);
+	void postOrder(Node*);
+	void levelOrder(Node*);
+	void walk(Node *);
+	void countBST();
 };
 
